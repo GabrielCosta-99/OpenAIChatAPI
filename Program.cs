85b101e49c1bf4abe6dbe359d0e623c7af9069b0
MattNogueira <matheus.nogueira@aglets.com.br>
2025-06-03 18:36:27 -0300

    refactor: arquivos considerados problemáticos removidos

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Lendo as configurações da API Key e do Assistant ID
-var openAiApiKey = builder.Configuration["OpenAI:ApiKey"]
-    ?? throw new Exception("OpenAI ApiKey não encontrada nas configurações");
+// Configurando o cliente do Key Vault
+var keyVaultUrl = "https://solimkey.vault.azure.net/"; // URL do Key Vault
+var secretClient = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+// Lendo as configurações da API Key e do Assistant ID do Key Vault
+var openAiApiKey = secretClient.GetSecret("SolimChaveAPI")?.Value
+    ?? throw new Exception("OpenAI ApiKey não encontrada no Key Vault");
 
 var assistantId = builder.Configuration["OpenAI:AssistantId"]
     ?? throw new Exception("AssistantId não encontrado nas configurações");
+
 
 // Registrando HttpClient para OpenAI com base na API Key e cabeçalho beta
 builder.Services.AddHttpClient("OpenAI", client =>
@@ -23,12 +31,12 @@ builder.Services.AddScoped(provider =>
     return new OpenAIThreadService(httpClient, assistantId);
 });
 
-// Configurando CORS para aceitar requisições do front (exemplo com Angular)
+// Configurando CORS para aceitar requisições do front 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", policy =>
     {
-        policy.WithOrigins("http://localhost:4200") // Atualize para a URL do seu front-end
+        policy.WithOrigins("http://localhost:4200") //  URL do front-end
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
